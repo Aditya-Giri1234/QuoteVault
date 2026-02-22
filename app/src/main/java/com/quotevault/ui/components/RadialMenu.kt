fb66b8b13b2e9bf4c8aa3ce5b74e970fb3a13e1e
LENOVO <adityagiri359@gmail.com>
2026-02-22 15:19:35 +0530

    Add doc about member extension function

--- app/src/main/java/com/quotevault/ui/components/RadialMenu.kt
+++ app/src/main/java/com/quotevault/ui/components/RadialMenu.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.pointerInput
@@ -462,6 +463,8 @@ fun RadialMenuCanvas(
                 top = iconCenter.y - scaledSize / 2
             ) {
                 with(painter) {
+                    // this draw is member extension function which reside Painter class. And this draw function extension of DrawScope.
+                    // Means access this draw function we need Drawscope and instance of painter class.
                     draw(
                         size = Size(scaledSize, scaledSize),
                         alpha = 1f,
